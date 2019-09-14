e097f4cb73773a2922c0715cc6a47001c300e8a7
zht <1208009695@qq.com>
2019-09-14 20:36:25 +0800

    第一次提交

---
+++ day15/src/Test.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+
+        //1.初始化大小为16的int类型数组(30分)
+        for(int a=1;a<=16;a++){
+            for(int b=1;b<a-1;b++){
+                System.out.println(a%b);
+            }
+            //2. 使用for循环给数组赋值（随机数）(30分)
+            String its=new String();
+            its.substring(its.lastIndexOf(Random));
+
+        }
+        //3. 使用冒泡排序对赋值后的数组进行降序排序（40分）
+        System.out.println(sum.desc);
+    }
+}
