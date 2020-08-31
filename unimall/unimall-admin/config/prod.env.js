96f30d7fee6fac214759d384e8d12ac5d1736d73
zht <1208009695@qq.com>
2020-08-31 15:57:54 +0800

    导入代码

---
+++ unimall/unimall-admin/config/prod.env.js
@@ -0,0 +1,6 @@
+module.exports = {
+	NODE_ENV: '"production"',
+	ENV_CONFIG: '"prod"',
+    HOST: '"https://demo.easycampus.cn"',
+  	BASE_API: '"https://demo.easycampus.cn/m.api"'
+}
---
+++ unimall/unimall-app/config/index.js
@@ -0,0 +1,11 @@
+const dev = {
+	baseUrl: 'http://127.0.0.1:8080',
+	// baseUrl: 'http://192.168.8.188:8081',
+	// baseUrl: 'https://demo.easycampus.cn',
+	h5Appid: 'wxb66b599f7f61b46f',
+	debug: false
+}
+
+export function def() {
+	return dev
+}
